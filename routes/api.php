696a60f109e4f62b67ebee7e6880801ec190967c
robertocalabrese <r.calabrese@edicomspa.it>
2024-01-02 22:14:41 +0100

    avanzamento

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 //TODO Creare una route API che esegue il dispatch dei job per lo scraping (ASYNC)
 //TODO Il Jon applica la logica di cache (se esiste in cache lo prendo, se non c'è lo prendo e lo mantengo per X minuti)
 //TODO aggiungere evento EMIT (websocket) usando pusher per aggiornare i dati lato client (logica chi arriva arriva).
 
-Route::get('/palermo', \App\Http\Controllers\ApiPalermoController::class);
+Route::get('/sicilia/palermo', \App\Http\Controllers\Sicilia\ApiPalermoController::class);
